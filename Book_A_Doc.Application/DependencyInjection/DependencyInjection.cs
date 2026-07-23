b3ffc0305d120563faf23b6bd811af9ffd92b4e8
Mahmoud Khalid <mahmoudkhalid29122005@gmail.com>
2026-07-23 18:01:08 +0300

    feat(application): configure dependency injection

---
+++ Book_A_Doc.Application/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Book_A_Doc.Application.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        // Add MediatR Services
+        services.AddMediatR(cfg =>
+            cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
+
+
+
+        //Add FluentValidation Services
+        services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
+
+
+
+        //
+
+        return services;
+    }
+
+    
+}
