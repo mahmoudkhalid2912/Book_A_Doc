89b9c28b9c5286e0f5e88a5623199493a30def44
Mahmoud Khalid <mahmoudkhalid29122005@gmail.com>
2026-07-25 19:46:54 +0300

    add api class response

---
+++ Book_A_Doc/ApiResponse/ApiResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace Book_A_Doc.ApiResponse;
+
+public class ApiResponse<T>
+{
+    public string Message { get; init; } = string.Empty;
+
+    public T? Data { get; init; }
+
+    public IEnumerable<ApiError>? Errors { get; init; }
+}
+
+public class ApiError
+{
+    public string Code { get; init; } = string.Empty;
+
+    public string Description { get; init; } = string.Empty;
+}
---
+++ Book_A_Doc/ApiResponse/ResultExtension.cs
@@ -0,0 +1,67 @@
+﻿using Book_A_Doc.ApiResponse;
+using Book_A_Doc.Domain.ResultPattern;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Book_A_Doc.API.Extensions;
+
+public static class ResultExtensions
+{
+    public static IActionResult ToApiResponse<T>(
+        this Result<T> result,
+        ControllerBase controller)
+    {
+        if (result.IsSuccess)
+        {
+            return controller.Ok(new ApiResponse<T>
+            {
+                Message = result.Message ?? "Success",
+                Data = result.Value
+            });
+        }
+
+        return new ObjectResult(new ApiResponse<T>
+        {
+            Message = result.Error.Description,
+            Errors =
+            [
+                new ApiError
+                {
+                    Code = result.Error.Code,
+                    Description = result.Error.Description
+                }
+            ]
+        })
+        {
+            StatusCode = result.Error.StatusCode
+        };
+    }
+
+    public static IActionResult ToApiResponse(
+        this Result result,
+        ControllerBase controller)
+    {
+        if (result.IsSuccess)
+        {
+            return controller.Ok(new ApiResponse<object>
+            {
+                Message = result.Message ?? "Success"
+            });
+        }
+
+        return new ObjectResult(new ApiResponse<object>
+        {
+            Message = result.Error.Description,
+            Errors =
+            [
+                new ApiError
+                {
+                    Code = result.Error.Code,
+                    Description = result.Error.Description
+                }
+            ]
+        })
+        {
+            StatusCode = result.Error.StatusCode
+        };
+    }
+}
