0c0a4903771bbcd30e08b374f6088a5ea99766a7
Mahmoud Khalid <mahmoudkhalid29122005@gmail.com>
2026-07-25 19:35:05 +0300

    Change Name of Stauts Code Variable

--- Book_A_Doc.Domain/ResultPattern/Error.cs
+++ Book_A_Doc.Domain/ResultPattern/Error.cs
@@ -1,6 +1,6 @@
 ﻿namespace Book_A_Doc.Domain.ResultPattern;
 
-public record Error(string Code, string Description, int? StatucCode)
+public record Error(string Code, string Description, int? StatusCode)
 {
     public static readonly Error None = new(string.Empty, string.Empty, null);
 }
