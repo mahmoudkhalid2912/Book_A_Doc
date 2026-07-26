0e80ce0eae77057b10c2f0d1f3931be8d27a9552
Mahmoud Khalid <mahmoudkhalid29122005@gmail.com>
2026-07-26 18:22:11 +0300

    Make BirthDay nullable and update validation handling

--- Book_A_Doc.Domain/Models/Identity/ApplicationUser.cs
+++ Book_A_Doc.Domain/Models/Identity/ApplicationUser.cs
@@ -6,7 +6,7 @@ public class ApplicationUser : IdentityUser<Guid>
 {
     public string FullName { get; set; } = string.Empty;
 
-    public DateOnly BirthDate { get; set; }
+    public DateOnly? BirthDate { get; set; }
 
     public string? ProfileImageUrl { get; set; }
 
