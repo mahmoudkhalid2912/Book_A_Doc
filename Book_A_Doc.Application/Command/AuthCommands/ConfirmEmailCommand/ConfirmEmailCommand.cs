c157b35e0ba8e651d1021edac77ae879baa26fc8
Mahmoud Khalid <mahmoudkhalid29122005@gmail.com>
2026-07-28 18:20:13 +0300

    Add email confirmation flow

---
+++ Book_A_Doc.Application/Command/AuthCommands/ConfirmEmailCommand/ConfirmEmailCommand.cs
@@ -0,0 +1,11 @@
+﻿using Book_A_Doc.Domain.ResultPattern;
+using MediatR;
+
+namespace Book_A_Doc.Application.Command.AuthCommands.ConfirmEmailCommand;
+
+public class ConfirmEmailCommand:IRequest<Result>
+{
+    public Guid UserId {  get; set; }
+
+    public string Token { get; set; }=string.Empty;
+}
