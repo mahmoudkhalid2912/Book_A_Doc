1f500f71788da48913b01d56ee8253d104a16fd2
Mahmoud Khalid <mahmoudkhalid29122005@gmail.com>
2026-07-30 10:42:39 +0300

    add mail options class to map configuration

---
+++ Book_A_Doc.Infrastructre/JwtServices/OptionsClass/MailOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace Book_A_Doc.Infrastructre.JwtServices.OptionsClass;
+
+public class MailOptions
+{
+    public string Mail { get; set; } = string.Empty;
+
+    public string DisplayName { get; set; } = string.Empty;
+
+    public string Password { get; set; } = string.Empty;
+
+    public string Host { get; set; } = string.Empty;
+
+    public int Port { get; set; }
+}
