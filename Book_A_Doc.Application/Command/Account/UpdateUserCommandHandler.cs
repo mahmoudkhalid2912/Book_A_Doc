9d87ff16c35e30dea508526c834d609e0742951a
Mahmoud Khalid <mahmoudkhalid29122005@gmail.com>
2026-08-04 19:01:37 +0300

    Feature/UpdateUserProfile

---
+++ Book_A_Doc.Application/Command/Account/UpdateUserCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using Book_A_Doc.Application.Services;
+using Book_A_Doc.Domain.ResultPattern;
+using Book_A_Doc.Domain.ResultPattern.ErrorMessage;
+using Book_A_Doc.Domain.ResultPattern.SuccessMessages;
+using MediatR;
+
+namespace Book_A_Doc.Application.Command.Account;
+
+public class UpdateUserCommandHandler(IIdentityService identityService):IRequestHandler<UpdateUserCommand,Result<bool>>
+{
+    public async Task<Result<bool>> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+    {
+        var user = await identityService.FindByIdAsync(request.UserId);
+
+        user!.FullName = request.Name;
+        if (!string.IsNullOrWhiteSpace(request.Phone))
+        {
+            user!.PhoneNumber = request.Phone;
+        }
+        if (request.BirthDate.HasValue)
+        {
+            user!.BirthDate = request.BirthDate.Value;
+        }
+        
+
+        var UpdatedResult = await identityService.UpdateUserAsync(user);
+        if (UpdatedResult.IsFailure)
+        {
+           return  Result.Failure<bool>(AccountErrors.UnableToUpdateUser);
+        }
+
+        return Result.Success<bool>(true,AccountMessages.UserUpdatedSuccessfully);
+    }
+}
---
+++ Book_A_Doc.Application/Command/Account/UpdateUserCommandValidation.cs
@@ -0,0 +1,42 @@
+﻿using Book_A_Doc.Domain.ResultPattern.ErrorMessage;
+using FluentValidation;
+
+namespace Book_A_Doc.Application.Command.Account;
+
+public class UpdateUserCommandValidation:AbstractValidator<UpdateUserCommand>
+{
+    public UpdateUserCommandValidation()
+    {
+        RuleFor(x => x.Name).NotEmpty().MaximumLength(100)
+            .WithMessage(AccountErrors.UserNameIsTooLong.Description);
+
+        When(x => x.BirthDate.HasValue, () =>
+        {
+            RuleFor(x => x.BirthDate)
+                .Must(BeAtLeast15YearsOld)
+                .WithMessage(AccountErrors.UserMustBeAtLeast15YearsOld.Description);
+        });
+
+        When(x => !string.IsNullOrWhiteSpace(x.Phone), () =>
+        {
+            RuleFor(x => x.Phone)
+                .Matches(@"^(?:\+20|0)1[0125]\d{8}$")
+                .WithMessage(AccountErrors.InvalidEgyptianPhoneNumber.Description);
+        });
+    }
+
+    private static bool BeAtLeast15YearsOld(DateOnly? birthDate)
+    {
+        if (!birthDate.HasValue)
+            return false;
+
+        var today = DateOnly.FromDateTime(DateTime.UtcNow);
+
+        var age = today.Year - birthDate.Value.Year;
+
+        if (birthDate.Value > today.AddYears(-age))
+            age--;
+
+        return age >= 15;
+    }
+}
