8dfa035cb2378af921a9767ebee08bf472d20629
Mahmoud Khalid <mahmoudkhalid29122005@gmail.com>
2026-08-06 11:50:20 +0300

    Implement OTP verification command

---
+++ Book_A_Doc.Application/Command/AuthCommands/VerifyOtpCommand/VerifyOtpCommand.cs
@@ -0,0 +1,11 @@
+﻿using Book_A_Doc.Domain.ResultPattern;
+using MediatR;
+
+namespace Book_A_Doc.Application.Command.AuthCommands.VerifyOtpCommand;
+
+public class VerifyOtpCommand:IRequest<Result>
+{
+    public string Email { get; set; } = string.Empty;
+
+    public string Code { get; set; }= string.Empty;
+}
