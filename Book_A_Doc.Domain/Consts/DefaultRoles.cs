72bfd05e9c045728436f73ecbfa56d04376fa933
Mahmoud Khalid <mahmoudkhalid29122005@gmail.com>
2026-08-09 11:53:19 +0300

    Apply Role_based Authorization

---
+++ Book_A_Doc.Domain/Consts/DefaultRoles.cs
@@ -0,0 +1,12 @@
+﻿namespace Book_A_Doc.Domain.Consts;
+
+public static class DefaultRoles
+{
+    public const string Admin = "Admin";
+    public const string Doctor = "Doctor";
+    public const string Patient = "Patient";
+
+    public const string AdminId = "019fe598-e849-7658-b1ca-7f456442ec9a";
+    public const string DoctorId = "019fe599-25dd-7f12-89a8-3d81b0e08e29";
+    public const string PatientId = "019fe599-7d99-7966-9717-a07c1edb1194";
+}
---
+++ Book_A_Doc.Domain/Consts/DefaultUser.cs
@@ -0,0 +1,22 @@
+﻿namespace Book_A_Doc.Domain.Consts;
+
+public static class DefaultUser
+{
+    public const string AdminId =
+        "019fe590-0d88-70da-9d85-a597550cea7b";
+
+    public const string AdminFullName =
+        "System Administrator";
+
+    public const string AdminEmail =
+        "admin@Book_A_Doc.com";
+
+    public const string AdminPassword =
+        "P@ssw0rd123";
+
+    public const string AdminSecurityStamp =
+        "019fe590-62c8-774a-8d3c-08e8ff500972";
+
+    public const string AdminConcurrencyStamp =
+        "019fe590-a2e9-7d36-b8ef-fcf7e4b6ec8b";
+}
