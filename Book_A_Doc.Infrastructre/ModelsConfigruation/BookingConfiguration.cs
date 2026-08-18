9aaca0942d5935a9d523ee5efbea58a53b5c7169
Mahmoud Khalid <mahmoudkhalid29122005@gmail.com>
2026-08-18 10:32:23 +0300

    feat(infrastructure): configure booking and payment entities

---
+++ Book_A_Doc.Infrastructre/ModelsConfigruation/BookingConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Book_A_Doc.Infrastructre.ModelsConfigruation;
+public class BookingConfiguration : IEntityTypeConfiguration<Booking>
+{
+    public void Configure(EntityTypeBuilder<Booking> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Status)
+               .IsRequired();
+
+        builder.Property(x => x.Amount)
+               .HasPrecision(18, 2)
+               .IsRequired();
+
+        builder.HasOne(x => x.Patient)
+               .WithMany()
+               .HasForeignKey(x => x.PatientId)
+               .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(x => x.Doctor)
+               .WithMany()
+               .HasForeignKey(x => x.DoctorId)
+               .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(x => x.AvailabilitySlot)
+               .WithOne(x => x.Booking)
+               .HasForeignKey<Booking>(x => x.AvailabilitySlotId)
+               .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(x => x.Payment)
+               .WithOne(x => x.Booking)
+               .HasForeignKey<Payment>(x => x.BookingId)
+               .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasIndex(x => x.AvailabilitySlotId)
+       .IsUnique()
+       .HasFilter(
+           $"[Status] IN ({(int)BookingStatus.PendingPayment}, {(int)BookingStatus.Confirmed})");
+    }
+}
---
+++ Book_A_Doc.Infrastructre/ModelsConfigruation/DoctorAvailabilityConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Book_A_Doc.Infrastructure.Persistence.Configurations;
+
+public class DoctorAvailabilityConfiguration
+    : IEntityTypeConfiguration<DoctorAvailability>
+{
+    public void Configure(EntityTypeBuilder<DoctorAvailability> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.DayOfWeek)
+               .IsRequired();
+
+        builder.Property(x => x.StartTime)
+               .IsRequired();
+
+        builder.Property(x => x.EndTime)
+               .IsRequired();
+
+        builder.Property(x => x.SlotDurationInMinutes)
+               .IsRequired();
+
+        builder.Property(x => x.IsActive)
+               .IsRequired();
+
+        builder.HasOne(x => x.Doctor)
+               .WithMany()
+               .HasForeignKey(x => x.DoctorId)
+               .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasIndex(x => new
+        {
+            x.DoctorId,
+            x.DayOfWeek,
+            x.StartTime,
+            x.EndTime
+        })
+        .IsUnique();
+    }
+}
